34b940ad9c7c7099940dbb3658ecc727a1fd1571
pankaj thakur <109215106+panku0607@users.noreply.github.com>
2024-02-07 14:04:44 +0530

    Add files via upload

---
+++ Arrays/Qno_35_searching_usingBinary.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class Qno_35_searching_usingBinary {
+
+    public static int searchInsert(int[] nums, int target) {
+            int start = 0 , end = nums.length-1;
+
+            while (start <= end) {
+                int mid = start + (end - start)/2 ; 
+                
+                if (nums[mid] == target) {
+                     return mid;
+                } else if(nums[mid] > target){
+                    end = mid -1 ;
+                } else {
+                    start = mid + 1 ;
+                }
+            }
+        return start  ;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,3,5,7};
+        int target = 9;
+        System.out.println(searchInsert(nums, target));
+    }
+}
